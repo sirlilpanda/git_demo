c66a4cb2cb1e519ac3e638837d8cda9c650903e7
sirlilpanda <me@sirlilpanda.studio>
2025-07-09 14:37:54 +1200

    minor changes

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 
 def main() -> None:
     print("hello world")
+    print("hello world")
+    print("hello world")
     print("goodbye world")
-    print("goodbye world")
+    print("goodbye world 2")
 
 # comment
 main()
