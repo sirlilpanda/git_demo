92390f1f1afa4c8d5ca3b35ce1a9f7087982acac
sirlilpanda <33928689+sirlilpanda@users.noreply.github.com>
2025-07-16 10:28:01 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 
 
+def OwnPrint(string : str) -> None:
+    print(string)
+
 
 def main() -> None:
-    print("hello world")
-    print("hello world")
-    print("hello world")
-    print("goodbye world")
-    print("goodbye world 2")
-    print("goodbye world 2")
-    print("goodbye world 42")
+    OwnPrint("hello world")
+    OwnPrint("hello world")
+    OwnPrint("hello world")
+    OwnPrint("goodbye world")
+    OwnPrint("goodbye world 2")
+    OwnPrint("goodbye world 2")
+    OwnPrint("goodbye world 42")
 
 # comment
 main()
