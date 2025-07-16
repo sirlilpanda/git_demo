427ebf6f71a7c8bfd3ae303be49d54ed00e29d47
sirlilpanda <33928689+sirlilpanda@users.noreply.github.com>
2025-07-16 13:06:12 +1200

    removed own print

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 
 
-def OwnPrint(string : str) -> None:
-    print(string)
-
-
 def main() -> None:
     print("hello world")
     print("hello world")
@@ -14,13 +10,6 @@ def main() -> None:
     print("goodbye world 2")
     print("goodbye world 2")
     print("goodbye world 42")
-    OwnPrint("hello world")
-    OwnPrint("hello world")
-    OwnPrint("hello world")
-    OwnPrint("goodbye world")
-    OwnPrint("goodbye world 2")
-    OwnPrint("goodbye world 2")
-    OwnPrint("goodbye world 42")
 
 # comment
 main()
